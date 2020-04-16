e0f661760885ca8fa066484910a0a2070adcd442
oktembaris77 <oktembaris77@users.noreply.github.com>
2020-04-16 09:40:01 +0300

    Dış map

---
+++ Assets/Proje/Yazılım/Dış Map/nav_dismap.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class nav_dismap : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav_control();
+    }
+
+    private void nav_control()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                nav.destination = hit.point;
+        }
+    }
+}
