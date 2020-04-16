ac3449c83ba5aabfe34a8dd22c45f08caeb402fe
Ekrem <eysterty@gmail.com>
2020-04-16 13:58:41 +0300

    SaatGun Sistemi - Van
    kanalıma hoşgeldiniz arkadaşlar

---
+++ Assets/Proje/Yazılım/Dış Map/gunSistem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gunSistem : MonoBehaviour
+{
+    private Text gunText;
+    public float gunYazi;
+
+    public GameObject saatObje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gunText = GetComponent<Text>();
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gunYazi = saatObje.GetComponent<saatSistem>().Gun;
+
+
+        gunText.text = "Day:" + gunYazi;
+    }
+}
---
+++ Assets/Proje/Yazılım/Dış Map/saatSistem.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class saatSistem : MonoBehaviour
+{
+    
+    private Text saatText;
+    
+
+    public float Timer,dakika,saniye,Gun;
+    void Awake()
+    {
+     /*   gunYazi = GameObject.Find("gunYazi");
+        saatYazi = GameObject.Find("saatYazi"); */
+
+        saatText = GetComponent<Text>();
+        
+    }
+    void Update()
+    {
+        
+
+        
+
+      dakika = Mathf.Floor(Timer / 60);       //SANİYEYİ DAKİKA GİBİ KULLAN GÜN DAHA HIZLI GEÇSİN
+      saniye = Mathf.Floor(Timer % 60);
+       Timer += Time.deltaTime *5;
+        //  string second = LeadingZero(time.Second);
+        if (Timer >= 0 && Timer <= 600)
+        {
+            saatText.text = "0" + dakika + ":" + saniye;
+
+
+        }
+
+        else
+        {
+            saatText.text = dakika + ":" + saniye;
+
+
+
+        }
+
+        if (saniye < 10)
+        {
+            saatText.text = "0" + dakika + ":" + "0" + saniye;
+
+
+
+
+        }
+
+
+
+        if (Timer >= 1440)
+        {                       
+            Timer = 0;
+            Gun += 1;
+            
+        } 
+    }
+    string LeadingZero(int n)
+    {
+        return n.ToString().PadLeft(2, '0');
+    }
+}
